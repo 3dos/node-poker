0de45436b0171c91126e4813909ca5518ab95455
3Dos <fildos@gmail.com>
2019-06-09 16:21:22 +0200

    wip: just to save the current state of it

--- specs/hand-generator.spec.js
+++ specs/hand-generator.spec.js
@@ -1,7 +1,8 @@
-const { describe } = require('riteway')
+const { describe, Try } = require('riteway')
 const R = require('ramda')
 
-const { Card, newDeck } = require('../lib/cards')
+const Games = require('../lib/games')
+const { deal, isADeck, newDeck, shuffle } = require('../lib/cards')
 
 describe('newDeck()', async assert => {
   const deck = newDeck()
@@ -16,7 +17,10 @@ describe('newDeck()', async assert => {
   assert({
     given: 'no arguments',
     should: 'return a deck of Card prototypes',
-    actual: deck.every(x => Card.isPrototypeOf(x)),
+    actual: deck.every(R.pipe(
+      R.toString,
+      R.match(/^Card\([A|2|3|4|5|6|7|8|9|T|J|Q|K][c|d|h|s]\)$/g))
+    ),
     expected: true
   })
 
@@ -25,5 +29,62 @@ describe('newDeck()', async assert => {
     should: 'return a deck of 52 different cards',
     actual: R.uniq(deck).length,
     expected: 52
+  })
+})
+
+describe('shuffle()', async assert => {
+  const deck = newDeck()
+
+  assert({
+    given: 'no argument',
+    should: 'throw',
+    actual: Try(shuffle()),
+    expected: new TypeError()
+  })
+
+  assert({
+    given: 'a deck',
+    should: 'return an actual deck',
+    actual: isADeck(shuffle(deck)),
+    expected: true
+  })
+
+  assert({
+    given: 'a deck',
+    should: 'return a different deck',
+    actual: JSON.stringify(shuffle(deck)) !== JSON.stringify(deck),
+    expected: true
+  })
+})
+
+describe('deal()', async assert => {
+  const deck = newDeck()
+
+  assert({
+    given: 'no argument',
+    should: 'throw',
+    actual: Try(deal()),
+    expected: new TypeError()
+  })
+
+  assert({
+    given: 'a Game type',
+    should: 'throw',
+    actual: Try(deal(Games.HOLDEM)),
+    expected: new TypeError()
+  })
+
+  assert({
+    given: 'a Game type and 2 players',
+    should: 'throw',
+    actual: Try(deal(Games.HOLDEM, 2)),
+    expected: new TypeError()
+  })
+
+  assert({
+    given: 'a Game type, 2 players and a deck',
+    should: 'return a hand',
+    actual: Array.isArray(deal(Games.HOLDEM, 2, deck)),
+    expected: true
   })
 })
